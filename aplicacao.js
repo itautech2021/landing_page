41bdc3876c8b06740f7163f237afb2d2cea3bae8
VanessaGomesdeOliveira <assenavsp@hotmail.com>
2021-07-20 23:27:23 -0300

    alterando formulario

--- aplicacao.js
+++ aplicacao.js
@@ -1,10 +1,3 @@
-function Enviar() { 
-
-    let nome = document.getElementById("name");
-    
-    console.log(nome);
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' a sua mensagem foi encaminhada com sucesso');
-    }
+function exibeAlert(){
+    alert('A sua mensagem foi encaminhada com sucesso');
 }
